c00af199f1f499b655a46b74e2b3fc48b73d31af
rfzu <rall@list.ru>
2015-06-22 12:47:36 +0300

    wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,17 @@
 class OrdersController < ApplicationController
 
   def show
+    DEBUG_LOG.info params
+    @order = Order.find_by(id: params[:id])
+    return redirect_to '/' unless @order
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    DEBUG_LOG.info params
   end
 
   def index
